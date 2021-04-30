8761d51bd44214c8059f6afaf0f00b84e65d5c7c
Olatunji S <teejayx@outlook.com>
2021-04-30 22:39:30 +0100

    switch statement

--- hello.go
+++ hello.go
@@ -3,5 +3,35 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("hello, world")
+	fmt.Println(Hello("Chris", ""))
+}
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Holla, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string{
+	if name == ""{
+		name = "World"
+	}
+
+	
+
+	return greetingPrefix(language) + name
+}
+
+
+func greetingPrefix(language string) (prefix string){
+
+switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+return 
+
+
 }
