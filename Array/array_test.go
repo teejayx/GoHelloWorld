d066177448a4c5837add992ccd35b39f0fef1191
Olatunji S <teejayx@outlook.com>
2021-05-02 18:09:01 +0100

    arrays

---
+++ Array/array_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+
+
+	t.Run("Collection of 5 numbers", func(t *testing.T){
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	want := 15
+	//%v for arrays
+
+	if got != want {
+		t.Errorf("got %d want %d  given, %v ", got, want, numbers)
+	}})
+
+
+	t.Run("collection of any size", func (t *testing.T)  {
+		
+        numbers := []int{1,2,3}
+
+		got:= SumExample(numbers)
+		want := 6
+
+		if got != want{
+			t.Errorf("got %d want %d  given, %v ", got, want, numbers)
+		}
+	})
+
+
+}
+
+//reflect.DeepEqual is useful for seeing if any two variables are the same 
+// not also typesafe
+func TestSumAll(t *testing.T){
+      got := SumAll([]int{1,3}, []int{0, 9})
+	  want := []int {3, 9}
+    
+	  if !reflect.DeepEqual(got, want) {
+		   t.Errorf("got %v want %v", got, want)
+	  }
+
+
+}
