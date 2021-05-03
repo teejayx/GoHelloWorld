dd05a47cf01b2b870ef33b20ec25ab57d0f72169
Olatunji S <teejayx@outlook.com>
2021-05-03 17:50:55 +0100

    struct

--- StructsMethodsInterfaces/Shapes.go
+++ StructsMethodsInterfaces/Shapes.go
@@ -2,8 +2,25 @@ package main
 
 
 
-func Perimeter(width float64 , height float64 ) float64{
+func Perimeter(rectangle Rectangle ) float64{
 
-     return  2 * (width + height)
+     return  2 * (rectangle.Width + rectangle.Height)
 
+}
+
+func Area (rectangle Rectangle) float64{
+	return rectangle.Height * rectangle.Width
+}
+
+
+
+
+
+
+
+//declaring a struct
+
+type Rectangle struct{
+	Width float64
+	Height float64
 }
